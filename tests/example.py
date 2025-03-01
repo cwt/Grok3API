a0d2b739e260115d8e2d664b040f4b061d479dc7
Alexey <boykopovar@gmail.com>
2025-03-01 19:38:29 +0300

    license and readme

---
+++ tests/example.py
@@ -0,0 +1,13 @@
+from grok3.client import GrokClient
+
+
+def main():
+    message = "Создай изображение корабля"
+    client = GrokClient()
+
+    result = client.ChatCompletion.create(message)
+    print("Ответ Grok:", result.modelResponse.message)
+    result.modelResponse.generatedImages[0].save_to("generated_image.jpg")
+
+if __name__ == '__main__':
+    main()
